98199a311658f264cc4b49a0d8374b41d47d8f47
aryabovich <aryabovich@uoc.edu>
2026-04-27 15:39:50 +0200

    Configuración del Stack, estilos de header y botón de Inicio
    Añadido Stack Navigator en App.js, configuración de estilos del header y botón de navegación a Inicio

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity } from "react-native";
 
 import DetailScreen from "./screens/DetailScreen";
 import InicioScreen from "./screens/InicioScreen";
@@ -11,14 +12,63 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Inicio" component={InicioScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#1E3A8A" },
+          headerTintColor: "#FFFFFF",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      >
+        <Stack.Screen 
+          name="Inicio" 
+          component={InicioScreen} 
+          options={{ title: "Equipo Basket" }} 
+        />
 
-        <Stack.Screen name="Listado" component={ListScreen} />
+        <Stack.Screen 
+          name="Listado" 
+          component={ListScreen} 
+          options={({ navigation }) => ({
+            title: "Listado de jugadores",
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Inicio")}>
+              <Text style={{ color: "#FFFFFF", marginRight: 10 }}>
+                Inicio
+              </Text>
+            </TouchableOpacity>
+            ),
+          })}
+        />
 
-        <Stack.Screen name="Detalle" component={DetailScreen} />
+        <Stack.Screen
+          name="Detalle"
+          component={DetailScreen}
+          options={({ navigation }) => ({
+            title: "Detalle del jugador",
+            headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Inicio")}>
+            <Text style={{ color: "#FFFFFF", marginRight: 10 }}>
+              Inicio
+            </Text>
+            </TouchableOpacity>
+            ),
+          })}
+        />
 
-        <Stack.Screen name="Media" component={MediaScreen} />
+        <Stack.Screen
+          name="Media"
+          component={MediaScreen}
+          options={({ navigation }) => ({
+          title: "Multimedia",
+            headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.popToTop()}>
+            <Text style={{ color: "#FFFFFF", marginRight: 10 }}>
+              Inicio
+            </Text>
+            </TouchableOpacity>
+          ),
+        })}
+      />
       </Stack.Navigator>
     </NavigationContainer>
   );
